a0f43a41dcf2aa4ffc37bbaa97759267befd1075
ronihere <74338794+ronihere@users.noreply.github.com>
2022-01-30 17:34:01 +0530

    Add files via upload

---
+++ englishdictionary/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from PyDictionary import PyDictionary
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def word(request):
+    search = request.GET.get('search')
+    dictionary = PyDictionary()
+    meaning = dictionary.meaning(search)
+    context = {
+        'meaning': meaning,
+        'search':search
+
+    }
+    return render(request, 'word.html', context)
